5b746382ae6bf814162e1be2deb2668687762e67
linux_china <linux_china@hotmail.com>
2016-02-25 21:22:47 +0800

    document constants

---
+++ src/main/java/org/mvnsearch/jsondoc/document/DocumentationConstants.java
@@ -0,0 +1,10 @@
+package org.mvnsearch.jsondoc.document;
+
+public class DocumentationConstants {
+
+    /* Groups */
+    public final static String GROUP_USER = "User";
+
+    public final static String AUTHORIZATION = "xxxx";
+
+}
